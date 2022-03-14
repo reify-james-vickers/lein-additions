85927ddbbc951d0969f99e802d231f120223f26c
reify-james-vickers <98407385+reify-james-vickers@users.noreply.github.com>
2022-03-14 16:47:53 -0600

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -10,14 +10,14 @@
     :pedantic? :warn
     ; not sure what this does but fixes: syntax errors compiling fipp/ednize.clj:1
     :eval-in :subprocess
-    :plugins [[lein-pprint "1.3.2"]
-              [lein-mutate "0.1.0"]]
+    :plugins [[lein-pprint "1.3.2"]]
     :dependencies [[hashp "0.2.1" :exclusions [org.clojure/tools.reader]]
                    [postmortem "0.5.0"]
                    [philoskim/debux "0.8.2"]
                    [vvvvalvalval/scope-capture "0.3.2"]
                    [org.clojure/tools.trace "0.7.11"]
                    [im.chit/lucid.core.inject "1.3.13" :exclusions [rewrite-clj]]
+                   [inspector-jay "0.3"]
                    [org.clojure/core.incubator "0.1.4"]]
     :injections [(require 'hashp.core)
                  (require '[lucid.core.inject :as inj])
@@ -25,7 +25,6 @@
                  (inj/inject-single 'clojure.core '_spy #'sc.api/spy)
                  (inj/in
                   clojure.core
-                  [clojure.inspector inspect inspect-tree inspect-table]
                   [postmortem.core log-for spy>> reset-key!]
                   [debux.core dbg dbgn]
                   [sc.api letsc defsc]
