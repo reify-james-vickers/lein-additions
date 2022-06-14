4c4f034fe0639ca058d39c2a008fa5c9278b3520
reify-james-vickers <james.vickers@reifyhealth.com>
2022-06-14 15:11:01 -0600

    Add portal setup to profile

--- profiles.clj
+++ profiles.clj
@@ -15,9 +15,12 @@
                    [philoskim/debux "0.8.2" :exclusions [args4j]]
                    [org.clojure/tools.trace "0.7.11"]
                    [im.chit/lucid.core.inject "1.3.13" :exclusions [rewrite-clj]]
+                   [djblue/portal "0.26.0"]
                    [org.clojure/core.incubator "0.1.4"]]
     :injections [(require 'hashp.core)
                  (require '[lucid.core.inject :as inj])
+                 (require '[portal.api :as portal])
+                 (portal/tap) ; add portal as a tap target
                  (inj/in
                   clojure.core
                   [debux.core dbg dbgn dbgt set-debug-mode! set-ns-whitelist! set-ns-blacklist!]
